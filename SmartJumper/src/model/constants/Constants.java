78ead819d4542aaa6a0be39fee54f58af941eb4a
Tuncer <Tuncer@Tuncer-PC.fritz.box>
2018-07-25 01:39:13 +0200

    Changed Gravity and Jump Constants

--- SmartJumper/src/model/constants/Constants.java
+++ SmartJumper/src/model/constants/Constants.java
@@ -6,11 +6,11 @@ public final class Constants {
 	
 	public static final double FPS = 1000d/60d;
 	public static final double MAX_JUMP_DISTANCE = 200;
-	public static final double GRAVITY = 2;
-	public static final double JUMP = -(GRAVITY+2);
+	public static final double GRAVITY = 3;
+	public static final double JUMP = -4;
 	
-	public static final int GROUNDENEMY_MIN_SPEED = 1;
+	public static final int GROUNDENEMY_MIN_SPEED = 2;
 	public static final int GROUNDENEMY_MAX_SPEED = 10;
 	public static final int FLYENEMY_MIN_SPEED = 6;
-	public static final int FLYENEMY_MAX_SPEED = 13;
+	public static final int FLYENEMY_MAX_SPEED = 12;
 }
